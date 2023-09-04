5a848183c5a46be916173f7ccb04bcb9fca548fa
Anton Volnuhin <anton@volnuhin.com>
2023-09-05 01:29:23 +0300

    auto-update

--- full-update.fish
+++ full-update.fish
@@ -12,7 +12,7 @@ cd /home/anton/ipnames
 
 fish names-from-cache.fish
 fish presolve.fish
-#fish presolve.fish -ext
+fish presolve.fish -ext
 git add full-update.fish updates.log names-from-cache.fish presolve.fish dns-*.txt resolve-*.txt ext-resolve-*.txt cache-ar*.txt ext-dns-*.txt servers*.txt
 git commit -m 'auto-update'
 git push https://{$GITHUB_TOKEN}@github.com/antonme/ipnames.git
