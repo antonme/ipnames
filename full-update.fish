fb684aea4d7705a9bd64c574fc1758511b6ac169
Anton Volnuhin <anton@volnuhin.com>
2023-09-04 04:55:28 +0300

    full-update

---
+++ full-update.fish
@@ -0,0 +1,7 @@
+fish names-from-cache.fish
+fish presolve.fish
+fish presolve.fish -ext
+git add names-from-cache.fish presolve.fish dns-*.txt resolve-*.txt ext-resolve-*.txt cache-ar*.txt ext-dns-*.txt servers*.txt
+git commit -m 'auto-update'
+git push
+
