5cd2100e2d580b2f600a22c6a3bee57c6e0efe97
Anton Volnuhin <anton@volnuhin.com>
2023-09-20 16:56:36 +0300

    backblaze and ipv6 fixes

---
+++ presolve6.fish
@@ -0,0 +1,43 @@
+# Initialize a counter variable
+set -g counter 0
+
+# Total number of tasks
+set -g total_tasks 0
+
+# Custom command to increment the counter and print progress
+
+function resolve -a name resolvers new_name concur
+	parallel -j $concur --bar host -t AAAA -W 3 :::: $name :::: $resolvers| rg address | awk '$5{print $5}' | sort -u| sort -h > $new_name
+end
+
+
+set prefixer ''
+set concur 16
+echo
+
+if not contains -- '-ext' $argv
+  echo "== Resolving names to ipv6"
+  set servers "servers.txt"
+else
+  echo "== Resolving external names to ipv6"
+  set prefixer 'ext-'
+  set servers "servers-ext.txt"
+  set concur 80
+end
+
+set names (ls "$prefixer"dns-*.txt)
+
+if test $argv[2]
+  set names "$prefixer"dns-$argv[2].txt
+end
+
+for name in $names
+	set name_array (string split '-' $name)
+	if test -z $prefixer
+  	  set new_name "ipv6-resolve-"$name_array[2]
+    else
+  	  set new_name "ipv6-ext-resolve-"$name_array[3]
+	end
+       echo (date)"                "$name"  >"$new_name
+  	resolve $name $servers $new_name $concur
+end
