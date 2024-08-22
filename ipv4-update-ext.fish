eace2763e68dfe31355d6a10cef0d82785eb20f3
Anton Volnuhin <anton@volnuhin.com>
2024-08-22 20:12:22 +0300

    more discrete updates

---
+++ ipv4-update-ext.fish
@@ -0,0 +1,29 @@
+#!/usr/bin/env fish
+
+echo
+echo
+echo
+echo --------------------------------------------------------------------------------
+date
+echo
+echo "==> Beginning ipnames update."
+echo
+echo --------------------------------------------------------------------------------
+
+cd /home/anton/ipnames
+export GCM_CREDENTIAL_STORE=gpg
+
+fish names-from-cache.fish
+
+
+git add full-update.fish update.log names-from-cache.fish presolve.fish dns-*.txt ipv6*.txt resolve-*.txt ext-resolve-*.txt cache-ar*.txt ext-dns-*.txt servers*.txt
+git commit -m 'auto-update names'
+git push https://{$GITHUB_TOKEN}@github.com/antonme/ipnames.git
+
+
+fish presolve.fish -ext
+
+
+git add full-update.fish update.log names-from-cache.fish presolve.fish dns-*.txt ipv6*.txt resolve-*.txt ext-resolve-*.txt cache-ar*.txt ext-dns-*.txt servers*.txt
+git commit -m 'auto-update ipv4'
+git push https://{$GITHUB_TOKEN}@github.com/antonme/ipnames.git
